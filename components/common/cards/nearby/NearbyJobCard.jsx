1cf20805fdf0c30f526abf2351a267a79cd826b3
KofouegFrank <kefoueg@dmail.com>
2023-08-25 15:58:13 +0200

    stater files added

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Home = () => {
+const NearbyJobCard = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <Text>NearbyJobCard</Text>
     </View>
   )
 }
 
-export default Home
+export default NearbyJobCard
