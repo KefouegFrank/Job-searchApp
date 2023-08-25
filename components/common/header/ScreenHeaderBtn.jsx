2d5c14dc5b2c784a844b5aca82aa2edf86b49909
KofouegFrank <kefoueg@dmail.com>
2023-08-25 18:12:47 +0200

    homescrenbtn component modified

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -7,7 +7,6 @@ const ScreenHeaderBtn = ({iconUrl, dimension, handlePress}) => {
   return (
     <TouchableOpacity style={styles.btnContainer} onPress={handlePress}>
       <Image source={iconUrl}
-      resizeMode='cover'
       style={styles.btnImg(dimension)}/>
     </TouchableOpacity>
   )
